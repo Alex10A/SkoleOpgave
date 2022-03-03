32968e3c0772ffd898a42650ac9509a2d355d4f1
janpe20 <71701712+janpe20@users.noreply.github.com>
2022-03-03 11:06:57 +0100

    Add project files.

---
+++ SkoleOpgave/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkoleOpgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            opgave opgave = new opgave();
+            opgave.Navn();
+            opgave.Klog();
+            opgave.Hvem();
+            opgave.navn2();
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SkoleOpgave/opgave.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SkoleOpgave
+{
+    public class opgave
+    {
+        public string Navn()
+        {
+            Console.WriteLine("Hvad hedder du ?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hej {name}");
+            return name;
+        }
+
+        public void Klog()
+        {
+            Console.WriteLine("Er du klog?");
+            string ErDuKlog = Console.ReadLine();
+            if (ErDuKlog.ToLower().Equals("ja"))
+            {
+                Console.WriteLine($"Er der andre der er kloge end dig ?");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Du er åbenbart ikke klog :/\nHvem er så klog?");
+                return;
+            }
+        }
+
+        public void Hvem()
+        {
+            string hvem = Console.ReadLine();
+            if (hvem.ToLower().Equals(""))
+            {
+                Console.WriteLine("Lukker.... Du skal svar :(");
+                Console.ReadKey();
+                Environment.Exit(0);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Hvad hedder personen?");
+                return;
+            }
+        }
+
+        public void navn2()
+        {
+           string navn2 = Console.ReadLine();
+           if (navn2.ToLower().Equals(""))
+            {
+                Console.WriteLine("Lukker.... Du skal svar :(");
+                Console.ReadKey();
+                Environment.Exit(0);
+                return;
+            } 
+           else
+            {
+                Console.WriteLine($"Jeg syntes også {navn2} er klog");
+                return;
+            }
+        }
+    }
+}
